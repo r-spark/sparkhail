6175b13e601df526053672d07ea5aeac171f7c5d
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-07-03 12:57:10 -0300

    mt_cols and mt_rows function

---
+++ R/utils.R
@@ -0,0 +1,25 @@
+#' @export
+get_methods <- function(jobj){
+  invoke(jobj, "getClass") %>% 
+    invoke("getMethods") 
+  
+  a2 <- lapply(a, function(e) {
+    invoke(e, "toString") 
+  })
+  
+}
+
+
+
+#' @importFrom sparklyr %>% invoke
+#' @export
+mt_cols <- function(jobj){
+  invoke(jobj, "countCols")
+}
+
+#' @importFrom sparklyr %>% invoke
+#' @export
+mt_rows <- function(jobj){
+  invoke(jobj, "countRows")
+}
+
