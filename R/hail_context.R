b749ae40e89d05084bd86e69c4becfcea22beea5
Javier Luraschi <javierluraschi@hotmail.com>
2019-07-11 11:22:09 -0700

    initialize hail context with spark context and helper hail_context()

--- R/hail_context.R
+++ R/hail_context.R
@@ -1,10 +1,5 @@
 #' @export
-hail_context <- function(spark_version = "2.4.0"){
-  conf <- sparklyr::spark_config()
-  conf$`spark.serializer` = "org.apache.spark.serializer.KryoSerializer"
-  conf$`spark.kryo.registrator` = "is.hail.kryo.HailKryoRegistrator"
-  sc <- sparklyr::spark_connect(master = "local", config = conf, version = spark_version)
-  
+hail_context <- function(sc) {
   sparklyr::invoke_static(sc, "is.hail.HailContext", "getOrCreate",
                           spark_context(sc), "Hail",
                           create_option_string(sc, "none"),
