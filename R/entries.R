f9586888d786d1be8894affc13b5dd5121037c7c
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-07-30 18:00:28 -0300

    add entries() function to get entries in mt

---
+++ R/entries.R
@@ -0,0 +1,12 @@
+#' @export
+entries <- function(df){
+  df %>% 
+    sparklyr.nested::sdf_select(call = entries.Call,
+                                ad = entries.AD,
+                                dp = entries.DP,
+                                gq = entries.GQ,
+                                pl = entries.PL) %>% 
+    sparklyr.nested::sdf_explode("call") %>% 
+    sparklyr.nested::sdf_explode("dp") %>% 
+    sparklyr.nested::sdf_explode("gq") 
+}
