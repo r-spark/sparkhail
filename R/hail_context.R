fe0034983d1e446bdca272fca6bc1bbf81de1a8e
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-08-02 09:53:08 -0300

    Add documentation

--- R/hail_context.R
+++ R/hail_context.R
@@ -1,3 +1,18 @@
+#' Create hail context
+#' 
+#' Import and initialize Hail using a spark connection.
+#'
+#' @param sc Spark connection.
+#' 
+#' @examples 
+#' \dontrun{
+#' library(sparklyr)
+#'
+#' sc <- spark_connect(master = "local", version = "2.4", config = hail_config())
+#'
+#' hail_context(sc)
+#' }
+#'
 #' @export
 hail_context <- function(sc) {
   sparklyr::invoke_static(sc, "is.hail.HailContext", "getOrCreate",
