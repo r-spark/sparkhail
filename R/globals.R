61c9dd6c57af51643200a03312d13bf0d3b52d89
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-08-02 15:51:26 -0300

    Set globals

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c(".",
+                         "entries.Call",
+                         "entries.AD",
+                         "entries.DP",
+                         "entries.GQ",
+                         "entries.PL"))
