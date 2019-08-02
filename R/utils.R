7932d1df175966a2188d6fc4ee5b951f9f016d88
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-08-02 15:55:09 -0300

    comment a piece of code test

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,12 @@
-get_methods <- function(jobj){
-  invoke(jobj, "getClass") %>% 
-    invoke("getMethods") 
-  
-  a2 <- lapply(a, function(e) {
-    invoke(e, "toString") 
-  })
-  
-}
+# get_methods <- function(jobj){
+#   invoke(jobj, "getClass") %>% 
+#     invoke("getMethods") 
+#   
+#   a2 <- lapply(a, function(e) {
+#     invoke(e, "toString") 
+#   })
+#   
+# }
 
 mt_cols <- function(jobj){
   sparklyr::invoke(jobj, "countCols")
