b9871bf152db6b9ed74574168b80e770053535d5
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-08-08 10:50:45 -0300

    remove hail_exists() from tests
    * it will be done just once in test-1-hail_install

--- tests/testthat/test-data_frame.R
+++ tests/testthat/test-data_frame.R
@@ -2,10 +2,9 @@ context("test-data_frame")
 
 test_that("hail_data_frame", {
   skip_on_cran()
-  skip_if(hail_exists() == "")
 
   sc <- sparklyr::spark_connect(master = "local", config = hail_config())
-  
+
   df <- hail_context(sc) %>% 
     hail_read_matrix(system.file("extdata/1kg.mt", package = "sparkhail")) %>% 
     hail_dataframe() %>% 
@@ -29,7 +28,7 @@ test_that("hail_entries", {
     head() %>% 
     sparklyr::collect()
 
-  expect_equal(df$locus[1], 4) 
+  expect_equal(df$call[1], 4) 
   expect_equal(df$ad[[1]][[1]][[1]], 4) # nested column
 })
 
